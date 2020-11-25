22667a5cef7fea4769504884b537094f29d21050
Ekojoka Christolight Idakwo <chris.idakwo@gmail.com>
2020-11-25 07:51:15 +0100

    - Added support for Laravel 8 - Updated chrisidakwo/flutterwave-php package - Updated other composer packages - Updated Rave facade: added method docs for newly supported services in `chrisidakwo/flutterwave-php`.
    Signed-off-by: Ekojoka Christolight Idakwo <chris.idakwo@gmail.com>

--- src/Facades/Rave.php
+++ src/Facades/Rave.php
@@ -2,6 +2,9 @@
 
 namespace ChrisIdakwo\Flutterwave\Laravel\Facades;
 
+use ChrisIdakwo\Flutterwave\Bank\BankAccountCharge\BankAccountCharge;
+use ChrisIdakwo\Flutterwave\Bank\BankTransfer\BankTransfer;
+use ChrisIdakwo\Flutterwave\BVN\BVNVerification;
 use ChrisIdakwo\Flutterwave\Transaction\Refund\Refund;
 use ChrisIdakwo\Flutterwave\Transaction\Transaction;
 use Illuminate\Support\Facades\Facade;
@@ -12,16 +15,25 @@ use Illuminate\Support\Facades\Facade;
  * @method static array standardPayment(array $data)
  * @method static Transaction verifyTransaction(string $transactionId)
  * @method static Refund refundTransaction(string $transactionId, int $amount)
+ * @method static BVNVerification verifyBVN(string $bvn)
+ * @method static array verifyBankAccount(string $bankCode, string $accountNumber)
+ * @method static BankTransfer bankTransfer(array $transactionData)
+ * @method static BankAccountCharge chargeBankAccount(array $transactionData)
+ * @method static Transaction chargeCardToken(string $cardToken, array $chargeData)
+ * @method static Transaction preAuthCardWithToken(string $cardToken, array $chargeData)
+ * @method static Transaction capturePreAuthCharge(string $flwRef, string $amount)
+ * @method static Transaction cancelPreAuthCard(string $flwRef)
+ * @method static Transaction refundPreAuthCharge(string $flwRef, string $amount)
  *
  * @package ChrisIdakwo\Flutterwave\Laravel\Facades
  */
 class Rave extends Facade {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor(): string {
-        return 'laravel-flutterwave';
-    }
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor(): string {
+		return 'laravel-flutterwave';
+	}
 }
